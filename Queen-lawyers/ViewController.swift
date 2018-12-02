7f33620922633328437a0bcf9891574d6a016564
Aboodslamehalathamneh <abood199944@gmail.com>
2018-12-02 14:25:18 +0200

    Initial Commit

---
+++ Queen-lawyers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Queen-lawyers
+//
+//  Created by Abdalrahman athamneh on 12/2/18.
+//  Copyright © 2018 Abdalrahman athamneh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
